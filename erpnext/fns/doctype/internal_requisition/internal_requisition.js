8411e3c0302d3a8a4e6067e0fac75630a698dc7c
Malika <thomasmalika91@gmail.com>
2023-12-28 01:46:17 +0000

    requisition form

--- erpnext/fns/doctype/internal_requisition/internal_requisition.js
+++ erpnext/fns/doctype/internal_requisition/internal_requisition.js
@@ -1,8 +1,47 @@
-// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Internal Requisition', {
-	// refresh: function(frm) {
+    onload: function(frm) {
+        frm.toggle_display('faculty_items', false);
+        frm.get_field('faculty_items').grid.cannot_add_rows = true;
+    },
+    faculty_button: function(frm) {
+        var isFacultyItemsVisible = frm.fields_dict.faculty_items.df.hidden;
+        frm.toggle_display('faculty_items', isFacultyItemsVisible);
 
-	// }
+        if (!isFacultyItemsVisible) {
+            copyDataToFacultyItems(frm);
+        }
+    }
 });
+
+function copyDataToFacultyItems(frm) {
+   
+    var childTableFieldNames = ['des_of_stock', 'quantity_req', 'uom'];
+    var facultyItemsTable = frm.fields_dict.faculty_items.grid;
+
+   
+    facultyItemsTable.remove_all();
+    childTableFieldNames.forEach(function(childTableName) {
+      
+        var internalItemsField = frm.fields_dict.internal_requisition_items[childTableName];
+
+        
+        if (internalItemsField) {
+            
+            for (var i = 0; i < internalItemsField.grid.get_data().length; i++) {
+                var row = internalItemsField.grid.get_data()[i];
+                var newRow = frm.add_child('faculty_items');
+
+              
+                newRow.des_of_stock = row.des_of_stock;
+                newRow.quantity_req = row.quantity_req;
+                newRow.uom = row.uom;
+
+
+                newRow.quantity_issued = 0; 
+                newRow.remarks = '';
+            }
+        }
+    });
+
+    frm.fields_dict.faculty_items.refresh();
+}
